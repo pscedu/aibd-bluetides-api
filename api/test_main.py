154d0040a05a966adf5fc6ab4e259f979c3f44a3
Wanting Huang <wanting@br005.pvt.bridges.psc.edu>
2020-07-04 23:56:42 -0400

    API: add test for /pig​/{id}/lengthbytype/{halo_id}/ and /pig​/{id}/lengthbytype​/n={num}

--- api/test_main.py
+++ api/test_main.py
@@ -53,6 +53,79 @@ def test_get_length_invalid_pig_id():
     assert response.status_code == 404
 
 
+### endpoint: /pig/{id}/lengthbytype/n={num}
+# Basic positive tests
+def test_get_lbt():
+    response = client.get("/pig/251/lengthbytype/n=4")
+    # Validate the status code: 200
+    assert response.status_code == 200
+    # Validate payload: Response is a well-formed JSON object and response data -- length should match the file data.
+    assert response.json() == {
+        "length_by_type": "[[446499, 507723, 0, 0, 561897, 7], [700225, 0, 0, 0, 1, 0], [239021, 247773, 0, 0, 173607, 8], [125966, 152736, 0, 0, 206388, 2]]"
+    }
+    # Validate headers
+    assert response.headers["content-type"] == "application/json"
+
+
+# Negative testing with invalid input
+# Missing required parameters
+def test_get_lbt_missing_input():
+    response = client.get("/pig/251/lengthbytype/")
+    # Validate the status code: 404
+    assert response.status_code == 404
+
+
+# Invalid value for endpoint parameters. E.g. halo_id not in [0,286036300) 
+def test_get_lbt_invalid_num():
+    response = client.get("/pig/251/lengthbytype/n=-1")
+    # Validate the status code: 400
+    assert response.status_code == 400
+
+
+# Invalid value for endpoint parameters. E.g. pig id not in PIG folder
+def test_get_lbt_invalid_pig_id():
+    response = client.get("/pig/200/lengthbytype/n=4")
+    # Validate the status code: 404
+    assert response.status_code == 404
+
+
+### endpoint: /pig/{id}/lengthbytype/{halo_id}/
+# Basic positive tests
+def test_get_lbh():
+    response = client.get("/pig/251/lengthbytype/100")
+    # Validate the status code: 200
+    assert response.status_code == 200
+    # Validate payload: Response is a well-formed JSON object and response data -- length should match the file data.
+    assert response.json() == {
+        "halo_id": 100,
+        "type_length": "[71937, 74788, 0, 0, 54739, 4]"
+    }
+    # Validate headers
+    assert response.headers["content-type"] == "application/json"
+
+
+# Negative testing with invalid input
+# Missing required parameters
+def test_get_lbh_missing_input():
+    response = client.get("/pig/251/lengthbytype/")
+    # Validate the status code: 404
+    assert response.status_code == 404
+
+
+# Invalid value for endpoint parameters. E.g. halo_id not in [0,286036300) 
+def test_get_lbh_invalid_haloid():
+    response = client.get("/pig/251/lengthbytype/-1")
+    # Validate the status code: 400
+    assert response.status_code == 400
+
+
+# Invalid value for endpoint parameters. E.g. pig id not in PIG folder
+def test_get_lbh_invalid_pig_id():
+    response = client.get("/pig/200/lengthbytype/5/")
+    # Validate the status code: 404
+    assert response.status_code == 404
+
+
 ### endpoint: /pig/{id}/offsetbytype/{halo_id}/
 # Basic positive tests
 def test_get_index():
