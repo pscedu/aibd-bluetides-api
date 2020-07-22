0ad1ae2111454ebde358c15de1e093f972fa9291
Wanting Huang <wanting@br005.pvt.bridges.psc.edu>
2020-07-22 13:56:37 -0400

    Tests: add star Generation positive tests

--- api/tests/test_star.py
+++ api/tests/test_star.py
@@ -31,3 +31,25 @@ def test_get_star_velocity_271():
     assert type(star_velocity) is list
     assert star_velocity[0] == [-36.19582748413086, -32.4619255065918, 53.46745681762695]
     assert len(star_velocity) == 622535
+
+
+### endpoint: /pig/{id}/star/Generation/{group_id}
+# Basic positive tests
+def test_get_star_generation_251():
+    response = client.get("/pig/251/star/Generation/2")
+    utils.common_positive_tests(response)
+    # Validate payload: Response is a well-formed JSON object and response data -- star Generation data should be a 1*1 array list
+    star_generation = json.loads(response.json()["star_generation"])
+    assert type(star_generation) is list
+    assert star_generation == [1]
+    assert len(star_generation) == 1
+
+
+def test_get_star_generation_271():
+    response = client.get("/pig/271/star/Generation/2")
+    utils.common_positive_tests(response)
+    # Validate payload: Response is a well-formed JSON object and response data -- star Generation data should be a 7*1 array list
+    star_generation = json.loads(response.json()["star_generation"])
+    assert type(star_generation) is list
+    assert star_generation == [1, 3, 1, 1, 1, 1, 1]
+    assert len(star_generation) == 7
