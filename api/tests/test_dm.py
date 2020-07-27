d0ea7848ae378cbfb58100eb4196177b8a823633
Wanting Huang <wanting@br006.pvt.bridges.psc.edu>
2020-07-27 14:29:24 -0400

    Tests: add more positive tests for dm

--- api/tests/test_dm.py
+++ api/tests/test_dm.py
@@ -64,6 +64,17 @@ def test_get_dm_velocity_271():
 
 
 #DM MASS
+def test_get_dm_mass_265():
+    response = client.get("/pig/265/dm/Mass/10")
+    utils.common_positive_tests(response)
+    # Validate payload: Response is a well-formed JSON object and response data -- gas position data should be a 158970*1 array list
+    data = json.loads(response.json()["dm_mass"])
+    assert type(data) is list
+    assert data[6] == 0.0011963852448388934
+    assert data[:4] == [0.0011963852448388934, 0.0011963852448388934, 0.0011963852448388934, 0.0011963852448388934]
+    assert len(data) == 153341
+
+
 def test_get_dm_mass_271():
     response = client.get("/pig/271/dm/Mass/10")
     utils.common_positive_tests(response)
@@ -76,6 +87,17 @@ def test_get_dm_mass_271():
 
 
 #DM POTENTIAL
+def test_get_dm_potential_244():
+    response = client.get("/pig/244/dm/Potential/10")
+    utils.common_positive_tests(response)
+    # Validate payload: Response is a well-formed JSON object and response data -- gas position data should be a 158970*1 array list
+    data = json.loads(response.json()["dm_potential"])
+    assert type(data) is list
+    assert data[6] == 109580.484375
+    assert data[:4] == [109690.15625, 109334.671875, 108381.1640625, 108467.40625]
+    assert len(data) == 120864
+
+
 def test_get_dm_potential_271():
     response = client.get("/pig/271/dm/Potential/10")
     utils.common_positive_tests(response)
@@ -88,6 +110,17 @@ def test_get_dm_potential_271():
 
 
 #DM GENERATION
+def test_get_dm_generation_244():
+    response = client.get("/pig/271/dm/Generation/100")
+    utils.common_positive_tests(response)
+    # Validate payload: Response is a well-formed JSON object and response data -- gas position data should be a 158970*1 array list
+    data = json.loads(response.json()["dm_generation"])
+    assert type(data) is list
+    assert data[0] == 0
+    assert data[:4] == [0, 0, 0, 0]
+    assert len(data) == 79204
+
+
 def test_get_dm_generation_271():
     response = client.get("/pig/271/dm/Generation/10")
     utils.common_positive_tests(response)
@@ -100,6 +133,17 @@ def test_get_dm_generation_271():
 
 
 #DM GROUPID
+def test_get_dm_groupid_244():
+    response = client.get("/pig/271/dm/GroupID/210")
+    utils.common_positive_tests(response)
+    # Validate payload: Response is a well-formed JSON object and response data -- gas position data should be a 158970*1 array list
+    data = json.loads(response.json()["dm_groupid"])
+    assert type(data) is list
+    assert data[0] == 210
+    assert data[:4] == [210, 210, 210, 210]
+    assert len(data) == 60163
+
+
 def test_get_dm_groupid_271():
     response = client.get("/pig/271/dm/GroupID/10")
     utils.common_positive_tests(response)
